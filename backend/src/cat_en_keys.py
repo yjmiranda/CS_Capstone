e4985f1778dcf061a18f8a1e3b1d78f2ca2cd6ac
Yali Miranda <ymiran7@wgu.edu>
2025-06-19 16:23:24 -0700

    Add decoder keys for encoded categorical features.

---
+++ backend/src/cat_en_keys.py
@@ -0,0 +1,22 @@
+# Keys for decoding encoded categorical features
+
+decode_employment_type = {
+    0:'Full-time',
+    1:'Part-time',
+    2:'Self-employed',
+    3:'Unemployed',
+}
+
+decode_marital_status = {
+    0:'Divorced',
+    1:'Married',
+    2:'Single'
+}
+
+decode_loan_purpose = {
+    0:'Auto',
+    1:'Business',
+    2:'Education',
+    3:'Home',
+    4:'Other'
+}
