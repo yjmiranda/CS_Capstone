06f0215617125ac7ae6925dbda2fa8e4158dcbd8
Yali Miranda <ymiran7@wgu.edu>
2025-06-24 17:55:38 -0700

    Add __pychache__ directory to gitignore

---
+++ backend/visuals/line_graph.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from config import CSV_FILE_PATH
+
+# define data frame
+loan_df = pd.read_csv(CSV_FILE_PATH)
+
+# bins for credit scores
+bins = [300, 500, 600, 650, 700, 750, 800, 850]
+labels = ['300-499', '500-599', '600-649', '650-699','700-749', '750-799', '800-850']
+loan_df['score-bin'] = pd.cut(loan_df['CreditScore'], bins=bins, labels=labels, include_lowest=True)
+
+# calculate default rate per bin
+default_rates = loan_df.groupby('score-bin', observed=True)['Default'].mean()
+
+# line plot for default rate per bin
+default_rates.plot(kind='line', marker='o')
+plt.title('Default Rate by Credit Score Bin')
+plt.xlabel('Credit Score Range')
+plt.ylabel('Default Rate')
+plt.grid(True)
+plt.tight_layout()
+plt.show()
